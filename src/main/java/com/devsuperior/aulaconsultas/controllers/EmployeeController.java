fae2f9bf9d3c385d09a435388c9cecb442bda07c
jopaulo666 <jp_cbc@hotmail.com>
2021-07-20 18:49:36 -0300

    Consultas ao banco de dados

---
+++ src/main/java/com/devsuperior/aulaconsultas/controllers/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.devsuperior.aulaconsultas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.aulaconsultas.dto.EmployeeDeptDTO;
+import com.devsuperior.aulaconsultas.services.EmployeeService;
+
+@RestController
+@RequestMapping(value = "/employees")
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService service;
+
+	@GetMapping(value = "/search1")
+	public ResponseEntity<List<EmployeeDeptDTO>> search1() {
+		List<EmployeeDeptDTO> list = service.search1("M", "Sales");
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/search2")
+	public ResponseEntity<List<EmployeeDeptDTO>> search2() {
+		List<EmployeeDeptDTO> list = service.search2("M", "Sales");
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/search3")
+	public ResponseEntity<List<EmployeeDeptDTO>> search3() {
+		List<EmployeeDeptDTO> list = service.search3("M", "Sales");
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/search4")
+	public ResponseEntity<List<EmployeeDeptDTO>> search4() {
+		List<EmployeeDeptDTO> list = service.search4("M", "Sales");
+		return ResponseEntity.ok(list);
+	}
+}
---
+++ src/main/java/com/devsuperior/aulaconsultas/services/EmployeeService.java
@@ -0,0 +1,44 @@
+package com.devsuperior.aulaconsultas.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.aulaconsultas.dto.EmployeeDeptDTO;
+import com.devsuperior.aulaconsultas.entities.Employee;
+import com.devsuperior.aulaconsultas.projections.EmployeeDeptProjection;
+import com.devsuperior.aulaconsultas.repositories.EmployeeRepository;
+
+@Service
+public class EmployeeService {
+
+	@Autowired
+	private EmployeeRepository repository;
+
+	@Transactional(readOnly = true)
+	public List<EmployeeDeptDTO> search1(String startName, String departmentName) {
+		List<Employee> result = repository.findByNameStartingWithAndDepartmentNameNot(startName, departmentName);
+		return result.stream().map(x -> new EmployeeDeptDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<EmployeeDeptDTO> search2(String startName, String departmentName) {
+		List<Employee> result = repository.search2(startName, departmentName);
+		return result.stream().map(x -> new EmployeeDeptDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional(readOnly = true)
+	public List<EmployeeDeptDTO> search3(String startName, String departmentName) {
+		return repository.search3(startName, departmentName);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<EmployeeDeptDTO> search4(String startName, String departmentName) {
+		List<EmployeeDeptProjection> result = repository.search4(startName, departmentName);
+		return result.stream().map(x -> new EmployeeDeptDTO(x)).collect(Collectors.toList());
+	}
+	
+}
